00afce1d56ccb9379e2001f4ab9e25cf5105f778
unknown <altamashbagwan1@gmail.com>
2021-11-24 23:32:50 +0530

    Added: Functioning of Calculating Wages For month

--- Employee_Wages.js
+++ Employee_Wages.js
@@ -5,8 +5,8 @@
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOURS = 20;
-
-    function getWorkingHours (empCheck) {
+    const NUM_OF_WORKING_DAYS = 20;
+    function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
                 return PART_TIME_HOURS;
@@ -16,8 +16,11 @@
                 return 0;
         }
     }
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs + WAGE_PER_HOURS;
-    console.log("Hours: " +empHrs+ " Emp Wage: " + empWage);
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs + WAGE_PER_HOURS;
+    console.log("Hours: " + totalEmpHrs + " Emp Wage: " + empWage);
 }
